5e0a77c6c6623c67a921818e7e3ccdd3c4647398
Yashmit Sagari <yashmitasd@gmail.com>
2026-01-30 22:04:28 +0530

    Start adding timers, added utils and fix bugs

--- code/groups.py
+++ code/groups.py
@@ -1,16 +1,14 @@
-from settings import *
+from settings import * 
 
 class AllSprites(pygame.sprite.Group):
-    def __init__(self,):
+    def __init__(self):
         super().__init__()
-        
-        # variables
         self.display_surface = pygame.display.get_surface()
         self.offset = pygame.Vector2()
-        
+
     def draw(self, target_pos):
-        self.offset.x = -(target_pos[0] - WINDOW_WIDTH /2)
-        self.offset.y = -(target_pos[1] - WINDOW_HEIGHT /2)
-        
+        self.offset.x = -(target_pos[0] - WINDOW_WIDTH / 2)
+        self.offset.y = -(target_pos[1] - WINDOW_HEIGHT / 2)
+
         for sprite in self:
             self.display_surface.blit(sprite.image, sprite.rect.topleft + self.offset)
